be796e51a1f20cbed8b3eb630c8a6b42c65c3eea
Anubhav Saini <iamanubhavsaini@gmail.com>
2016-03-25 12:50:52 +0530

    Whitespace killing commit - no really

--- models/index.js
+++ models/index.js
@@ -5,6 +5,6 @@ var teamMate1 = require('./teammate1');
 module.exports = function IndexModel() {
     return {
         name: teamMate1.name,
-	yow: teamMate1.yearOfWorkInWeeks
+        yow: teamMate1.yearOfWorkInWeeks
     };
 };
